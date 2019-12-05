e004e9193908adcb011735fdad0ebc51891beaaa
Seth Black <sblack@sethserver.com>
2019-12-05 08:24:31 -0600

    Continue work on gradient descent.

--- logreg.py
+++ logreg.py
@@ -2,6 +2,8 @@ import math
 import matplotlib.pyplot as plt
 import random
 
+from sklearn import metrics
+
 # https://en.wikipedia.org/wiki/Activation_function
 
 def sigmoid(x):
@@ -14,10 +16,6 @@ def sigmoid2(x):
 
 def sigmoid3(x):
   return 1 / (1 + math.exp(-x))
-
-
-def output(m, x, b):
-    return (m * x) + b
 
 
 def cross_entropy(predicted, actual):
@@ -39,15 +37,15 @@ def gen_fake_data(observations=100, intercept=.35, fuzz=.06):
     return d
 
 
-def plot_sigmoid(m, b):
+def plot_sigmoid(m):
     SX = []
     SY = []
 
-    for n in list(range(-10, 10)):
+    for n in list(range(0, 11)):
         np = n * .1
 
-        SX.append(n)
-        SY.append(sigmoid(output(m, np, b)))
+        SX.append(np)
+        SY.append(sigmoid(m * np))
 
     return (SX, SY)
 
@@ -57,47 +55,74 @@ def main():
     test = gen_fake_data(observations=20)
 
     m = 1.
-    b = 1.
-    learning_rate = 0.01
+    learning_rate = 0.1
 
     X, Y = zip(*train)
 
-    plt.scatter(X, Y, c=Y, cmap='rainbow')
+    # plt.scatter(X, Y, c=Y, cmap='rainbow')
 
-    # SX, SY = plot_sigmoid(m, b)
+    # SX, SY = plot_sigmoid(m)
     # plt.plot(SX, SY, color='blue')
 
-    for epoch in range(2):
+    previous_error = 0.
+
+    JX = []
+    JY = []
+
+    for epoch in range(10):
         print(f'epoch {epoch}')
-        sum_error = 0
+
+        sum_error = 0.
+        squared_error = 0.
+        predictions = []
+        actuals = []
 
         for t in train:
             x = t[0]
-            predicted = sigmoid(output(m, x, b))
+            predicted = sigmoid(m * x)
 
             actual = t[1]
 
             delta_m = abs(predicted - actual)
             sum_error += delta_m
+            squared_error += (actual - predicted) ** 2
 
-            print(predicted, actual, x, delta_m, m, b)
+            predictions.append(predicted)
+            actuals.append(x)
+
+            # print(predicted, actual, x, delta_m, m)
 
         mae1 = sum_error / len(train)
         mae2 = (1. / len(train)) * sum_error
+
+        mse1 = squared_error / len(train)
+        mse2 = metrics.mean_squared_error(actuals, predictions)
+
         print('mean abs err', mae1, mae2)
+        print('mean sq err', mse1, mse2)
 
-        # num_correct = 0.
+        m -= learning_rate * (previous_error - mae1)
 
-        # for t in test:
-        #     predicted = sigmoid(output(m, t[0], b))
+        print('new m', m, learning_rate, previous_error, mae1)
+        JX.append(mae1)
+        JY.append(m)
 
-        #     if predicted == t[1]:
-        #         num_correct += 1.
+        previous_error = mae1
 
-        # print('accuracy', num_correct / len(test), m, b)
+        num_correct = 0.
 
-    # SX, SY = plot_sigmoid(m, b)
-    # plt.plot(SX, SY, color='green')
+        for t in test:
+            predicted = sigmoid(m * t[0])
+
+            if predicted == t[1]:
+                num_correct += 1.
+
+        print('accuracy', num_correct / len(test), m)
+
+        # SX, SY = plot_sigmoid(m)
+        # plt.plot(SX, SY, color='green')
+
+    plt.plot(JX, JY, color='purple')
 
     plt.savefig('file.png')
 
